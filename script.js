707f5559e6c3569535f6c9d9e4f5995d7fb27af8
souldev003 <mehrabhosen003@gmail.com>
2026-02-22 16:28:39 +0600

    interview btn and interview count compeleted

--- script.js
+++ script.js
@@ -9,9 +9,11 @@ const mainDiv = document.querySelector("main");
 const allBtn = document.getElementById("all-btn");
 const interviewBtn = document.getElementById("interview-btn");
 const rejectBtn = document.getElementById("reject-btn");
-const jobListSection = document.getElementById("job-list-content");
+const jobListSection = document.getElementById("job-list-container");
+
 const interviewSection = document.getElementById("interview-section");
-const rejectSection = document.getElementById("reject-section");
+const rejectedSection = document.getElementById("rejected-section");
+const emptySection = document.getElementById("empty-section");
 
 const interviewCount = document.getElementById("interview-job");
 const rejectedJob = document.getElementById("rejected-job");
@@ -20,43 +22,65 @@ const rejectedJob = document.getElementById("rejected-job");
 
 //to make active btn colorful using javaScript
 function seeJob(id) {
+  //All three btn will be reset
   allBtn.classList.remove("stay-blue");
   interviewBtn.classList.remove("stay-blue");
   rejectBtn.classList.remove("stay-blue");
 
-  const selectedId = document.getElementById(id);
+  //All three section will be hidden
+  // jobListSection.classList.add("hidden");
+  // filterSection.classList.add("hidden");
+  // emptySection.classList.add("hidden");
 
-  selectedId.classList.add("stay-blue");
+  //target btn will be stay blue color
+  document.getElementById(id).classList.add("stay-blue");
 }
 
 //Main Logic Here:
 mainDiv.addEventListener("click", function (event) {
-  const grandParentNode = event.target.parentNode.parentNode;
-  const companyName = grandParentNode.querySelector(".company-name").innerText;
-  const jobTittle = grandParentNode.querySelector(".job-tittle").innerText;
-  const deleteBtn = grandParentNode.querySelector(".delete-icon");
-  const jobSalary = grandParentNode.querySelector(".job-salary").innerText;
-  const jobStatus = grandParentNode.querySelector(".job-status-btn").innerText;
-  const jobDetails = grandParentNode.querySelector(".job-details").innerText;
+  if (event.target.classList.contains("interview-selection-btn")) {
+    const grandParentNode = event.target.parentNode.parentNode;
+    const companyName =
+      grandParentNode.querySelector(".company-name").innerText;
+    const jobTittle = grandParentNode.querySelector(".job-tittle").innerText;
+    const deleteBtn = grandParentNode.querySelector(".delete-icon");
+    const jobSalary = grandParentNode.querySelector(".job-salary").innerText;
+    const jobStatus =
+      grandParentNode.querySelector(".job-status-btn").innerText;
+    const jobDetails = grandParentNode.querySelector(".job-details").innerText;
 
-  console.log(
-    companyName,
-    jobTittle,
-    deleteBtn,
-    jobSalary,
-    jobStatus,
-    jobDetails,
-  );
+    const jobCard = {
+      companyName,
+      jobTittle,
+      deleteBtn,
+      jobSalary,
+      jobStatus,
+      jobDetails,
+    };
 
-  // if (event.target.innerText == "INTERVIEW") {
-  //   interviewArray.push(parentDiv);
+    // console.log(jobCard);
 
-  //   totalJobCount();
-  // } else if (event.target.innerText == "REJECTED") {
-  //   rejectedArray.push(parentDiv);
+    const checkJobDeclared = interviewArray.find(
+      (item) =>
+        item.companyName === jobCard.companyName &&
+        item.jobTittle === jobCard.jobTittle,
+    );
 
-  //   totalJobCount();
-  // }
+    grandParentNode.querySelector(".job-status-btn").innerText =
+      "Interview Called";
+    grandParentNode
+      .querySelector(".job-status-btn")
+      .classList.add("btn-success");
+
+    if (!checkJobDeclared) {
+      interviewArray.push(jobCard);
+      totalJobCount();
+    }
+  }
+
+  if (event.target.classList.contains("reject-selection-btn")) {
+    console.log("reject btn clicked");
+  }
 });
 
 //Calculating all job
