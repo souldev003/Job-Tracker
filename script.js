04c6697d31bf543b5446596eb5c90f0ca6214bfb
souldev003 <mehrabhosen003@gmail.com>
2026-02-22 13:20:41 +0600

    fixed some bugs and restarted making logics

--- script.js
+++ script.js
@@ -1,11 +1,73 @@
-//Focus Btn Logic
-const allBtn = document.getElementById("all-btn");
-const mainContentDiv = document.getElementById("main-content-div");
+//Creating Two Blank Array to count interview or rejected value
+let interviewArray = [];
+let rejectedArray = [];
 
-document.addEventListener("click", function (event) {
-  if (allBtn.contains(event.target) || mainContentDiv.contains(event.target)) {
-    allBtn.classList.add("stay-blue");
-  } else {
-    allBtn.classList.remove("stay-blue");
-  }
+//Accessing all the item we need:
+
+const mainDiv = document.querySelector("main");
+
+const allBtn = document.getElementById("all-btn");
+const interviewBtn = document.getElementById("interview-btn");
+const rejectBtn = document.getElementById("reject-btn");
+const jobListSection = document.getElementById("job-list-content");
+const interviewSection = document.getElementById("interview-section");
+const rejectSection = document.getElementById("reject-section");
+
+const interviewCount = document.getElementById("interview-job");
+const rejectedJob = document.getElementById("rejected-job");
+
+//Making logic:
+
+//to make active btn colorful using javaScript
+function seeJob(id) {
+  allBtn.classList.remove("stay-blue");
+  interviewBtn.classList.remove("stay-blue");
+  rejectBtn.classList.remove("stay-blue");
+
+  const selectedId = document.getElementById(id);
+
+  selectedId.classList.add("stay-blue");
+}
+
+//Main Logic Here:
+mainDiv.addEventListener("click", function (event) {
+  const grandParentNode = event.target.parentNode.parentNode;
+  const companyName = grandParentNode.querySelector(".company-name").innerText;
+  const jobTittle = grandParentNode.querySelector(".job-tittle").innerText;
+  const deleteBtn = grandParentNode.querySelector(".delete-icon");
+  const jobSalary = grandParentNode.querySelector(".job-salary").innerText;
+  const jobStatus = grandParentNode.querySelector(".job-status-btn").innerText;
+  const jobDetails = grandParentNode.querySelector(".job-details").innerText;
+
+  console.log(
+    companyName,
+    jobTittle,
+    deleteBtn,
+    jobSalary,
+    jobStatus,
+    jobDetails,
+  );
+
+  // if (event.target.innerText == "INTERVIEW") {
+  //   interviewArray.push(parentDiv);
+
+  //   totalJobCount();
+  // } else if (event.target.innerText == "REJECTED") {
+  //   rejectedArray.push(parentDiv);
+
+  //   totalJobCount();
+  // }
 });
+
+//Calculating all job
+function totalJobCount() {
+  document.getElementById("totalJob").innerText =
+    jobListSection.children.length;
+  document.getElementById("totalJobCount").innerText =
+    jobListSection.children.length;
+
+  interviewCount.innerText = interviewArray.length;
+  rejectedJob.innerText = rejectedArray.length;
+}
+
+totalJobCount();
